d1a4f8ae3630f42ab20c6575a66182ce1c8f5736
retryW <retryw01@gmail.com>
2019-09-13 11:05:33 +1000

    Removed commented out lines from previous iterations.

--- Do-Progress.psm1
+++ Do-Progress.psm1
@@ -14,29 +14,22 @@ function Do-Progress([int]$count,[int]$total, [int]$started) {
 
     for($i = 0; $i -le $percentComplete; $i++) {
         if ($i -eq 24) {
-            #Write-Host $displayPercent.ToString()[0] -NoNewline -BackgroundColor Green -ForegroundColor Black
             $completeString += $displayPercent.ToString()[0]
         } elseif ($i -eq 25){
-            #Write-Host $displayPercent.ToString()[1] -NoNewline -BackgroundColor Green -ForegroundColor Black
             $completeString += $displayPercent.ToString()[1]
         } elseif ($i -eq 26){
             if($displayPercent -lt 100) {
-                #Write-Host "%" -NoNewline -BackgroundColor Green -ForegroundColor Black
                 $completeString += "%"
             } else {
-                #Write-Host $displayPercent.ToString()[2] -NoNewline -BackgroundColor Green -ForegroundColor Black
                 $completeString += $displayPercent.ToString()[2]
             }
         } elseif ($i -eq 27){
             if($displayPercent -eq 100) {
-                #Write-Host "%" -NoNewline -BackgroundColor Green -ForegroundColor Black
                 $completeString += "%"
             } else {
-                #Write-Host "-" -NoNewline -BackgroundColor Green -ForegroundColor Green
                 $completeBarPost += "-"
             }
         } else {
-            #Write-Host "-" -NoNewline -BackgroundColor Green -ForegroundColor Green
             if ($i -lt 24) {
                 $completeBarPre += "-"
             } else {
@@ -49,29 +42,22 @@ function Do-Progress([int]$count,[int]$total, [int]$started) {
         [int]$percentStarted = $percentStarted / 2
         for(; $i -le $percentStarted; $i++) {
             if ($i -eq 24) {
-                #Write-Host $displayPercent.ToString()[0] -NoNewline -BackgroundColor Yellow -ForegroundColor Black
                 $startedString += $displayPercent.ToString()[0]
             } elseif ($i -eq 25){
-                #Write-Host $displayPercent.ToString()[1] -NoNewline -BackgroundColor Yellow -ForegroundColor Black
                 $startedString += $displayPercent.ToString()[1]
             } elseif ($i -eq 26){
                 if($displayPercent -lt 100) {
-                    #Write-Host "%" -NoNewline -BackgroundColor Yellow -ForegroundColor Black
                     $startedString += "%"
                 } else {
-                    #Write-Host $displayPercent.ToString()[2] -NoNewline -BackgroundColor Yellow -ForegroundColor Black
                     $startedString += $displayPercent.ToString()[2]
                 }
             } elseif ($i -eq 27){
                 if($displayPercent -eq 100) {
-                    #Write-Host "%" -NoNewline -BackgroundColor Yellow -ForegroundColor Black
                     $startedString += "%"
                 } else {
-                    #Write-Host "-" -NoNewline -BackgroundColor Yellow -ForegroundColor Yellow
                     $startedBarPost += "-"
                 }
             } else {
-                #Write-Host "-" -NoNewline -BackgroundColor Yellow -ForegroundColor Yellow
                 if ($i -lt 24) {
                     $startedBarPre += "-"
                 } else {
@@ -82,16 +68,12 @@ function Do-Progress([int]$count,[int]$total, [int]$started) {
     }
     for(;$i -le 50; $i++) {
         if ($i -eq 24) {
-            #Write-Host $displayPercent.ToString()[0] -NoNewline -BackgroundColor Red -ForegroundColor White
             $incompleteString += $displayPercent.ToString()[0]
         } elseif ($i -eq 25){
-            #Write-Host $displayPercent.ToString()[1] -NoNewline -BackgroundColor Red -ForegroundColor White
             $incompleteString += $displayPercent.ToString()[1]
         } elseif ($i -eq 26){
-            #Write-Host "%" -NoNewline -BackgroundColor Red -ForegroundColor White
             $incompleteString += "%"
         } else {
-            #Write-Host "_" -NoNewline -BackgroundColor Red -ForegroundColor Red
             if ($i -lt 24) {
                 $incompleteBarPre += "-"
             } else {
